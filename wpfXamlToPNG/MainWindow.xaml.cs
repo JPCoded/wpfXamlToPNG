7360da0a2e8aff52e48ee9eaca3ff419625a900f
John Pomeroy <jpcoded@gmail.com>
2017-08-28 15:25:06 -0400

    Code cleanup and set the C# version to 4.7

--- wpfXamlToPNG/MainWindow.xaml.cs
+++ wpfXamlToPNG/MainWindow.xaml.cs
@@ -1,25 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Color = System.Drawing.Color;
-using Pen = System.Drawing.Pen;
 
 namespace wpfXamlToPNG
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -34,7 +19,7 @@ namespace wpfXamlToPNG
             var pen = new Pen(Color.FromArgb(255, 0, 0, 0));
             var bmp = new Bitmap(100, 100);
             var gr = Graphics.FromImage(bmp);
-            gr.DrawLine(pen,0,0,50,50);
+            gr.DrawLine(pen, 0, 0, 50, 50);
             bmp.Save(@"D:\test.png");
         }
     }
