63881c731a2090d2a22b999286ffe3d8f1ce966d
John Pomeroy <jpcoded@gmail.com>
2017-09-04 15:43:41 -0400

    Created a class to hold the datapoints

---
+++ wpfXamlToPNG/DataPoints.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfXamlToPNG
+{
+    public class DataPoints
+    {
+        private string command;
+        private double pointone;
+        private double pointtwo;
+
+        public DataPoints( string Points)
+        {
+
+        }
+
+        (double, double) GetPoints() => (pointone, pointtwo);
+
+      public  void SetPoints(string PointsToSet)
+        {
+            
+        }
+    }
+}
--- wpfXamlToPNG/MainWindow.xaml.cs
+++ wpfXamlToPNG/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
 using System.Windows;
 
 namespace wpfXamlToPNG
@@ -7,7 +11,7 @@ namespace wpfXamlToPNG
     ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {                        
         public MainWindow()
         {
             InitializeComponent();
@@ -16,11 +20,13 @@ namespace wpfXamlToPNG
         private void btnDraw_Click(object sender, RoutedEventArgs e)
 
         {
+            var datapoints = txtXaml.Text.Split(' ');
             var pen = new Pen(Color.FromArgb(255, 0, 0, 0));
-            var bmp = new Bitmap(100, 100);
+            var bmp = new Bitmap(Convert.ToInt32(txtWidth.Text), Convert.ToInt32(txtHeight.Text));
             var gr = Graphics.FromImage(bmp);
+            var dataPoints = datapoints.Select(point => new DataPoints(point)).ToList();
             gr.DrawLine(pen, 0, 0, 50, 50);
             bmp.Save(@"D:\test.png");
         }
     }
-}
+}     
